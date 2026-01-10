debaf74220c5c0016441affde2bdb959e8c7f516
Akanksha <akankshagarg2363@gmail.com>
2026-01-10 01:20:23 -0500

    Arrays:Q153: Find minimum in rotated sorted array

--- arrays/217_contains_duplicates.py
+++ arrays/217_contains_duplicates.py
@@ -1,4 +1,7 @@
 #Easy
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         set_nums = set(nums)
--- arrays/53_max_subarray.py
+++ arrays/53_max_subarray.py
@@ -1,5 +1,7 @@
 #Medium
-
+"""
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+"""
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # Remove negative prefix - Sliding Window
